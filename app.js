aa001ab1bc5d4eda4f101cc63614870cec693b15
Durumyisking <cww9512@gmail.com>
2022-12-09 15:08:32 +0900

    배경 렌더링, 플레이어 애니메이션 추가, 총알생성

--- app.js
+++ app.js
@@ -22,12 +22,7 @@ document.body.appendChild(app.view);
 
 
 // 윈도우 내부 컨테이너생성
-const container = new PIXI.Container();
-app.stage.addChild(container);
 
-// 컨테이너의 위치를 윈도우 중앙으로
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
 
 const Graphics = PIXI.Graphics;
 
@@ -36,6 +31,9 @@ const Graphics = PIXI.Graphics;
 const renderContainer = new PIXI.Container(1000, {
 
 });
+// 컨테이너의 위치를 윈도우 중앙으로
+renderContainer.x = -5;
+renderContainer.y = -5;
 app.stage.addChild(renderContainer);
 
 
@@ -44,17 +42,25 @@ const State = {
     IDLE: 0,
     MOVE: 1,
     JUMP : 2,
-    ATTACK: 3,
+    ATTACK: 4,
 }
-
+const DIR = {
+    NONE: 0,
+    U: 1,
+    D: 2,
+    L: 4,
+    R: 8,
+}
 //// functions ////
 
   const key_left = keyboardMgr("ArrowLeft"),
   key_up = keyboardMgr("ArrowUp"),
   key_right = keyboardMgr("ArrowRight"),
   key_down = keyboardMgr("ArrowDown"),
-  key_space = keyboardMgr(" ");
-
+  key_space = keyboardMgr(" "),
+  key_z = keyboardMgr("z"),
+  key_x = keyboardMgr("x"),
+  key_c = keyboardMgr("c");
 
 /** _target의 x y에 Sprite 추가 */
 function addSprite(_target , _texture, _x, _y)
@@ -111,6 +117,7 @@ function GameObj () {
     {
         this.vPos = Vec2(_x, _y);
     }
+
     this.SetScake = function(_x, _y) 
     {
         this.vScale = Vec2(_x, _y);
@@ -126,16 +133,17 @@ function GameObj () {
 
     }
 
-    this.CreateAnimation = function(name, path, maxframe) 
+    this.CreateAnimation = function(name, path, maxframe, loop) 
     {
-        const newAnim = new Animation(this, name, maxframe);
+        const newAnim = new Animation(this, name, maxframe, loop);
 
-        for(let i = 1; i< maxframe; ++i)
+        for(let i = 1; i<= maxframe; ++i)
         {
             const texture = PIXI.Texture.from(path + `${i}.png`);
             newAnim.Texture.push(texture);
         }
         newAnim.CreateAnim(this.vPos.x, this.vPos.y);
+
         this.Animations.push(newAnim);
     }
 
@@ -195,17 +203,79 @@ function GameObj () {
 }
 
 
+
+const Obj_Type = {
+    Floor: 0,
+    Player: 1,
+    Bullet: 2,
+    END: 10,
+}
+
+
+function Scene () {
+
+    this.arrGameObj = Array.from(new Array(Obj_Type.END), () => new Array(0));
+    this.Wintexture = PIXI.Texture.from('/DurumyJSEngine/images/bg/thekingsleap/Background/Windows/windows.png');
+    this.Farbgtexture = PIXI.Texture.from('/DurumyJSEngine/images/bg/thekingsleap/Background/Far/far_bg.png');
+    this.Columntexture = PIXI.Texture.from('/DurumyJSEngine/images/bg/thekingsleap/Background/Columns/columns.png');
+    this.Maintexture = PIXI.Texture.from('/DurumyJSEngine/images/bg/thekingsleap/Midground/Main.png');
+    this.Winsprite = new PIXI.Sprite(this.Wintexture);
+    this.Farbgsprite = new PIXI.Sprite(this.Farbgtexture);
+    this.Columnsprite = new PIXI.Sprite(this.Columntexture);
+    this.Mainsprite = new PIXI.Sprite(this.Maintexture);
+
+    //[Obj_Type.END][255];
+
+    this.update = function() 
+    {
+        for(var i =0; i< this.arrGameObj.length; ++i)
+        {
+            for(var j =0; j< this.arrGameObj[i].length; ++j)
+            {
+                if(this.arrGameObj[i][j] != null)
+                    this.arrGameObj[i][j].update();
+
+                if(null != this.arrGameObj[i][j].Collider)
+                    this.arrGameObj[i][j].Collider.update();
+            }
+        }
+    }
+    this.render = function(container) 
+    {
+       container.addChild(this.Farbgsprite);
+       container.addChild(this.Winsprite);
+       container.addChild(this.Columnsprite);
+       container.addChild(this.Mainsprite);
+
+        for(var i =0; i< this.arrGameObj.length; ++i)
+        {            
+            for(var j =0; j< this.arrGameObj[i].length; ++j)
+            {
+                if(this.arrGameObj[i][j] != null)
+                    this.arrGameObj[i][j].render(container);
+
+
+                if(null != this.arrGameObj[i][j].Collider)
+                    this.arrGameObj[i][j].Collider.render(container);
+            }
+        }
+    }
+}
+
+
 function Cuphead()
 { 
     this.name = "Player";
     this.State = State.IDLE;
     this.Onfloor = false;
     this.vScale = Vec2(100, 100);
+    this.vDir = DIR.NONE;
     this.fJumpTime = 0.0;
+    this.fAttackDelay = 0.0;
 
     this.update = function() 
     {
-        if(this.State == State.JUMP)
+        if((this.State & State.JUMP) == State.JUMP)
         {
             if(this.fJumpTime > 300.0)
             {
@@ -218,7 +288,7 @@ function Cuphead()
         }
         if(!this.Onfloor) // 바닥에 없을때
         {
-            if(this.State == State.JUMP)
+            if((this.State & State.JUMP) == State.JUMP) // 점프상태일때
                this.fJumpTime += MS;    
             else
             {
@@ -231,17 +301,26 @@ function Cuphead()
             case State.IDLE:
                 cuphead.changeAnim("cuphead_idle");
                 break;
-
+            case State.JUMP:
+                cuphead.changeAnim("cuphead_jump");
+                break;    
             case State.MOVE:
                 cuphead.changeAnim("cuphead_run");
                 break;
-
-            case State.JUMP:
-                cuphead.changeAnim("cuphead_jump");
+            case State.ATTACK:
+                cuphead.changeAnim("cuphead_aim_str");
                 break;
+            case State.ATTACK | State.MOVE:
+                cuphead.changeAnim("cuphead_run_shoot_str");
+                break;
+        }
+        if((this.State & State.JUMP) == State.JUMP)
+        {
+            cuphead.changeAnim("cuphead_jump");
         }
 
         this.Move();
+        this.Attack();
         
     }
     this.render = function(container) 
@@ -258,45 +337,150 @@ function Cuphead()
         key_right.press = () => {
             this.vPos.x += 5;
         };
-        key_space.press = () => {
-            if(this.State != State.JUMP && this.Onfloor)
-            {
-                this.State = State.JUMP;
-            }
+        key_c.press = () => { 
+            this.State |= State.JUMP;          
         };
+        
+        if(((this.State & State.ATTACK) == State.ATTACK) && !((this.State & State.JUMP) == State.JUMP) && !((this.State & State.MOVE) == State.MOVE))
+        {            
+            if(key_up.isDown)
+            {
+                cuphead.changeAnim("cuphead_aim_up");
+            }
+            if(key_down.isDown)
+            {
+                cuphead.changeAnim("cuphead_aim_down");
+            }
 
+            if(key_up.isUp && key_down.isUp)
+            {
+                cuphead.changeAnim("cuphead_aim_str");
+            }
+        }
+
+        // dir oper
+        //////////////////////////////////////////////////
         if(key_left.isDown)
         {
             this.vPos.x -= 5 * DT;
+            this.vDir |= DIR.L;
+            this.vDir &= ~DIR.R;
             this.CurrentAnim.Animsprite.scale.x = -1;
         }
         if(key_right.isDown)
         {
             this.vPos.x += 5 * DT;
+            this.vDir |= DIR.R;
+            this.vDir &= ~DIR.L;
             this.CurrentAnim.Animsprite.scale.x = 1;
         }
+        if(key_up.isDown)
+        {
+            this.vDir |= DIR.U;
+        }
+        if(key_down.isDown)
+        {
+            this.vDir |= DIR.D;
+        }
+
+        // if(key_left.isUp)
+        // {
+        //     this.vDir &= ~DIR.L;
+        // }
+        // if(key_right.isUp)
+        // {
+        //     this.vDir &= ~DIR.R;
+        // }
+        if(key_up.isUp)
+        {
+            this.vDir &= ~DIR.U;
+        }
+        if(key_down.isUp)
+        {
+            this.vDir &= ~DIR.D;
+        }
+        /////////////////////////////////////////////////////
 
         if(key_left.isDown || key_right.isDown)
         {
-            if(this.State != State.JUMP)
-                this.State = State.MOVE;
+            this.State |= State.MOVE;          
         }
+
 
         if(key_left.isUp && key_right.isUp)
         {
-            if(this.State != State.JUMP)
-                this.State = State.IDLE;
-        }
+            this.State &= ~State.MOVE;          
+        }   
+
+
         this.CurrentAnim.Animsprite.position.set(this.vPos.x, this.vPos.y + (this.vScale.y / 2));
 
     }
     
+    this.Attack = function ()
+    {
+        this.fAttackDelay += MS;
+
+        if(key_x.isDown)
+        {
+            if(this.fAttackDelay > 100)
+            {
+                this.CreateBullet(Vec2(this.vPos.x, this.vPos.y));
+                this.fAttackDelay = 0;
+            }
+        }
+
+
+        key_x.press = () => {
+            this.State |= State.ATTACK; 
+            if(this.fAttackDelay > 100)
+            {
+                var bulletDir = Vec2(0, 0);
+
+                switch(this.vDir)
+                {
+                    case DIR.U:
+                        bulletDir = Vec2(0, -1);
+                        break;
+                    case DIR.D:
+                        bulletDir = Vec2(0, 1);
+                        break;                       
+                    case DIR.L:
+                        bulletDir = Vec2(-1, 0);
+                        break;
+                    case DIR.R:
+                        bulletDir = Vec2(1, 0);
+                        break;  
+                    case DIR.U | DIR.L:
+                        bulletDir = Vec2(-1, -1);
+                        break;
+                    case DIR.U | DIR.R:
+                        bulletDir = Vec2(1, -1);
+                        break;  
+                    case DIR.D | DIR.L:
+                        bulletDir = Vec2(-1, 1);
+                        break;
+                    case DIR.D | DIR.R:
+                        bulletDir = Vec2(1, 1);
+                        break;  
+                }
+
+                this.CreateBullet(Vec2(this.vPos.x, this.vPos.y), bulletDir);
+                this.fAttackDelay = 0;
+            }
+        };
+
+        if(key_x.isUp)
+        {
+            this.State &= ~State.ATTACK;          
+        }
+    }
 
     this.OnCollisionEnter = function(_Other) 
     {
         if(_Other.Owner.name == "Floor")
         {
-            this.State = State.IDLE;
+            this.State &= ~State.JUMP;
             this.Onfloor = true;
             this.fJumpTime = 0;
 
@@ -312,36 +496,106 @@ function Cuphead()
             this.Onfloor = false;
         }
     }
+
+    this.CreateBullet = function(_vPos, _vDir)
+    {
+        var bullet = new Bullet(_vPos, _vDir);
+        GameScene.arrGameObj[Obj_Type.Bullet].push(bullet);
+    }
+}
+
+
+
+function Bullet(_vPos, _vDir)
+{
+    this.name = "Bullet";
+    this.vScale = Vec2(64, 32);
+    this.SetPos(_vPos.x, _vPos.y);
+    this.vDir = _vDir;
+    this.CreateAnimation("bullet_create", "/DurumyJSEngine/images/bullet/create/bullet_create_", 4, false);
+    this.CreateAnimation("bullet", "/DurumyJSEngine/images/bullet/bullet/bullet_", 8, true);
+    this.CreateAnimation("bullet_dead", "/DurumyJSEngine/images/bullet/bullet/bullet_", 6, false);
+    this.CreateCollider(this, this.vSclae);
+    this.playAnim("bullet_create");
+
+
+    this.update = function()
+    {
+        this.vPos.x = this.vPos.x * (5 * this.vDir.x * DT);
+        this.vPos.y = this.vPos.y * (5 * this.vDir.y * DT);
+
+        if(this.CurrentAnim.name == "bullet_create")
+        {
+            this.CurrentAnim.Animsprite.onComplete = () => {
+                this.changeAnim("bullet");
+            };    
+        }
+
+        if(this.CurrentAnim.name == "bullet")
+        {
+            this.CurrentAnim.onComplete = () => {
+                this.changeAnim("bullet_dead");
+            };    
+        }
+
+        if(this.CurrentAnim.name == "bullet_dead")
+        {
+            this.CurrentAnim.onComplete = () => {
+                this.stopAnim();
+                //delete this;
+            };    
+        }
+        this.CurrentAnim.Animsprite.position.set(this.vPos.x, this.vPos.y + (this.vScale.y / 2));
+    }
+    this.render = function(container) 
+    {
+    }
+
+    this.OnCollisionEnter = function(_Other) 
+    {
+        if(_Other.Owner.name == "Floor")
+        {
+            this.changeAnim("bullet_dead");
+        }
+    }
+    this.OnCollision = function(_Other) 
+    {
+    }
+    this.OnCollisionExit = function(_Other) 
+    {
+
+    }
 }
 
 
 function Floor()
 {
     this.name = "Floor";
-    this.texture = PIXI.Texture.from('/images/floor/floor.png');
-    this.sprite = new PIXI.Sprite(this.texture);
-    this.vScale = Vec2(1024, 64);
-    this.sprite.anchor.set(0.5, 1);
+    // this.texture = PIXI.Texture.from('/DurumyJSEngine/images/bg/floor.png');
+    // this.sprite = new PIXI.Sprite(this.texture);
+    this.vScale = Vec2(2048, 64);
+    // this.sprite.anchor.set(0.5, 1);
 
     this.update = function() 
     {
-        this.sprite.position.set(this.vPos.x, this.vPos.y + (this.vScale.y / 2));
+        // this.sprite.position.set(this.vPos.x, this.vPos.y + (this.vScale.y / 2));
     }
     this.render = function(container) 
     {
-        container.addChild(this.sprite);
+        // container.addChild(this.sprite);
     }
 
 }
 
 
-function Animation(_Owner, _Name, _MaxFrame)
+function Animation(_Owner, _Name, _MaxFrame, _loop)
 {
     this.Owner = _Owner
     this.name = _Name;
     this.Texture = [];
     this.MaxFrame = _MaxFrame;
     this.Animsprite = null;
+    this.loop = _loop;
 
     this.playAnim = function() 
     {
@@ -361,6 +615,7 @@ function Animation(_Owner, _Name, _MaxFrame)
         this.Animsprite = new PIXI.AnimatedSprite(this.Texture);
         this.Animsprite.position.set(_x, _y + (this.Owner.vScale.y / 2));
         this.Animsprite.anchor.set(0.5, 1);
+        this.Animsprite.loop = this.loop;
 
     }
 }
@@ -553,6 +808,7 @@ function CollisionMgr(_GameScene)
 
 Cuphead.prototype = new GameObj();
 Floor.prototype = new GameObj();
+Bullet.prototype = new GameObj();
 
 
 var GameScene = new Scene();
@@ -564,20 +820,25 @@ var cuphead = new Cuphead();
 cuphead.SetPos(100, 100);
 cuphead.CreateCollider(100, 100);
 var floor = new Floor();
-floor.SetPos(512, 500);
-floor.CreateCollider(1024, 64);
+floor.SetPos(512, 700);
+floor.CreateCollider(2048, 64);
 
 
 // 애니메이션 추가
-cuphead.CreateAnimation("cuphead_idle", "/images/cuphead_idle/cuphead_idle_", 9);
-cuphead.CreateAnimation("cuphead_run", "/images/cuphead_run/cuphead_run_", 16);
-cuphead.CreateAnimation("cuphead_jump", "/images/cuphead_jump/cuphead_jump_", 8);
+cuphead.CreateAnimation("cuphead_idle", "/DurumyJSEngine/images/cuphead_idle/cuphead_idle_", 9 , true);
+cuphead.CreateAnimation("cuphead_run", "/DurumyJSEngine/images/cuphead_run/cuphead_run_", 16, true);
+cuphead.CreateAnimation("cuphead_run_shoot_str", "/DurumyJSEngine/images/cuphead_run/shooting/cuphead_run_shoot_", 16, true);
+cuphead.CreateAnimation("cuphead_jump", "/DurumyJSEngine/images/cuphead_jump/cuphead_jump_", 8, true);
+cuphead.CreateAnimation("cuphead_aim_str", "/DurumyJSEngine/images/cuphead_aim/Straight/cuphead_aim_straight_", 5, true);
+cuphead.CreateAnimation("cuphead_aim_up", "/DurumyJSEngine/images/cuphead_aim/Up/cuphead_aim_up_", 5, true);
+cuphead.CreateAnimation("cuphead_aim_down", "/DurumyJSEngine/images/cuphead_aim/Down/cuphead_aim_down_", 5, true);
 
 cuphead.playAnim("cuphead_idle");
 
 // OBj추가
 GameScene.arrGameObj[Obj_Type.Floor].push(floor);
 GameScene.arrGameObj[Obj_Type.Player].push(cuphead);
+
 
 
 
@@ -595,7 +856,6 @@ app.ticker.add(delta => loop(delta));
 function loop(delta)
 {
     DT = delta;
-//    console.log(app.ticker.elapsedMS);
     MS = app.ticker.elapsedMS;
     
     renderContainer.removeChildren(); // 화면 클리어
@@ -606,12 +866,6 @@ function loop(delta)
 
     GameScene.render(renderContainer);
 
-    // const myrect = new Graphics();
-    // myrect.beginFill(0, 0.1)
-    // .lineStyle(1)
-    // .drawRect(100, 200, 100, 100)
-    // .endFill();
-    // renderContainer.addChild(myrect);
 
 }
 
--- mylib/Scene.js
+++ mylib/Scene.js
@@ -1,3 +1,5 @@
+
+
 const Obj_Type = {
     Floor: 0,
     Player: 1,
@@ -10,6 +12,8 @@ const Obj_Type = {
 var Scene = (function (exports) {
     'use strict';
     this.arrGameObj = Array.from(new Array(Obj_Type.END), () => new Array(0));
+    this.Wintexture = PIXI.Texture.from('/DurumyJSEngine/images/bg/thekingsleap/Background/Windows/windows.png');
+    this.Farbgtexture = PIXI.Texture.from('/DurumyJSEngine/images/bg/thekingsleap/Background/Far/far_bg.png');
     //[Obj_Type.END][255];
 
     this.update = function() 
@@ -28,6 +32,8 @@ var Scene = (function (exports) {
     }
     this.render = function(container) 
     {
+        container.addChild(this.Farbgtexture);
+        container.addChild(this.Wintexture);
 
         for(var i =0; i< this.arrGameObj.length; ++i)
         {            
